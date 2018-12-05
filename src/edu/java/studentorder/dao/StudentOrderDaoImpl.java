427f7dca1d98e9cf2bd35366adfee3676ed7726b
Boris Goncharov <bora4bora@gmail.com>
2018-12-04 22:56:27 -0500

    Transactions

--- src/edu/java/studentorder/dao/StudentOrderDaoImpl.java
+++ src/edu/java/studentorder/dao/StudentOrderDaoImpl.java
@@ -100,8 +100,9 @@ public class StudentOrderDaoImpl implements StudentOrderDao
             for (Child child : so.getChildren()) {
                 stmt.setLong(1, soId);
                 setParamsForChild(stmt, child);
-                stmt.executeUpdate();
+                stmt.addBatch();
             }
+            stmt.executeBatch();
         }
     }
 
